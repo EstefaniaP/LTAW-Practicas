0c974c0a61c5606e3db998e4e627045ebd7d3c08
Estefania Portillo <estefaniaportillocastro@gmail.com>
2022-04-29 00:07:48 +0200

    Arreglado error

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,72 +1,64 @@
-//Importando módulos
+//Importación módulos
 const url = require('url');
 const http = require('http');
 const fs = require('fs');
 
-//Definición del puerto
 const PUERTO = 9090;
 
-//Creación del servidor
-const server = http.createServer(function(req, res) {
+//Creación servidor
+const server = http.createServer((req, res) => {
 
-  console.log("Petición recibida");
+    console.log("Petición recibida")
 
-  //Construir objeto url con la url de la solicitud
-  let url = new URL(req.url, 'http://' + req.headers['host']);
-  console.log("Esta es la URL solicitada:" + url.href);
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    console.log(url.pathname);
 
-  //Se inicializa la variable recurso
-  let resource = ""; 
-  
-  //Analizar el recurso solicitado
-  if (url.pathname == '/') {
-    resource += "/tienda.html"; //Si pide la página principal
-  } else if(url.pathname == "/favicon.icon"){
-    file = 'ico.ico'
-  } else {
-    resource += url.pathname; //Si pide otro recurso
-  }
+    var resource = "";
+    
+    if (url.pathname == '/') {
+      resource += "/tienda.html"; //Solita página principal
+    } else {
+      resource += url.pathname; //Otro recurso
+    }
 
-  //Obtención tipo recurso solicitado
-  resource_type = resource.split(".")[1]; //la extension
-  resource = "." + resource;
+    //Obtención del recurso solicitado
+    resource_type = resource.split(".")[1];
+    resource = "." + resource;
 
-  console.log("Recurso: " + resource);
-  console.log("Extensión: " + resource_type);
+    console.log("Recurso: " + resource);
+    console.log("Extensión: " + resource_type);
 
+    //Lectura asíncrona
+    fs.readFile(resource, function(err, data){
 
-  //Definicion de todos los tipos de archivo
-  const mime = {
-    "html" : "text/html",
-    "jpeg" : "image/jpeg",
-    "jpg" : "image/jpg",
-    "png" : "image/png",
-    "PNG" : "image/PNG",
-    "ico" : "image/ico",
-    "css" : "text/css",
-  };
-  //Extrae tipo mime
-  let mime_type = mime[resource_type];
-  console.log("Tipo de mime asociado es:" + mime_type);
+      //Definición del tipo de archivo html.
+      var mime = "text/html"
 
-  //Lectura sincrona
-  fs.readFile(resource, function(err,data) {
-    //Fichero no encontrado
-    if (err){
-      //Lanza error
-      res.writeHead(404,{'Content-Type': 'text/html'})
-      console.log("Petición rechazada: 404 Not Found");
-      resource = "html/error.html";
-      data = fs.readFileSync(resource);
-      res.write(data);
-      res.end();
-    }else{
-      res.writeHead(200, {'Content-Type': mime});
-      console.log("Peticion Recibida, 200 OK");
-      res.write(data);
-      res.end();
-    }     
-  });
+      //Definición del tipo de imágenes
+      if(resource_type == 'jpg' || resource_type == 'png' || resource_type == 'PNG' || resource_type == 'gif'){
+          mime = "image/" + resource_type;
+      }
+
+      //Definición del tipo de archivo css
+      if (resource_type == "css"){
+          mime = "text/css";
+      }
+
+      //Fichero no encontrado
+      if (err) {
+        resource = "./error.html";
+        data = fs.readFileSync(resource);
+        res.writeHead(404, {'Content-Type': mime});
+        console.log("404 Not Found");
+        res.write(data);
+        res.end();
+      }else{
+          res.writeHead(200, {'Content-Type': mime});
+          console.log("Peticion Recibida, 200 OK");
+          res.write(data);
+          res.end();
+      }
+    });
 });
 
 server.listen(PUERTO);
