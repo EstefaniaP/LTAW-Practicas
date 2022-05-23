fd869e49b473dab407cd644491f3aa2aaf45f566
Estefania Portillo <estefaniaportillocastro@gmail.com>
2022-05-23 21:03:19 +0200

    Funciona todo menos nombre usuario

--- P3/chat-server.js
+++ P3/chat-server.js
@@ -7,11 +7,13 @@ const colors = require('colors');
 const PUERTO = 9090;
 
 let welcome_message = "¡Bienvenido al chat!";
-let new_user = "Se ha conectado un nuevo usuario al chat";
+let new_user = "Nuevo usuario conectado";
+let desconected = 'Un usuario abandonó el chat';
 let hello_message = "¡Hola!";
 let date = new Date (Date.now());
+var options = { year: 'numeric', month: 'long', day: 'numeric' };
 
-let commands = "Los comandos especiales son: <br> " +
+let commands = "Comandos especiales: <br> " +
                 "/help: Mostrará una lista con todos los comandos soportados <br>" + 
                 "/list: Devolverá el número de usuarios conectados <br>" +
                 "/hello: El servidor nos devolverá el saludo <br>" + 
@@ -29,7 +31,7 @@ const server = http.Server(app);
 const io = socket(server);
 
 //ENTRADA DE LA APLICACION WEB
-//Definimos punto de entrada principal de mi aplicación web
+//Punto de entrada de aplicación web
 app.get('/', (req, res) => {
   res.send('¡Bienvenido al chat!' + '<p><a href="/chat.html">Unirse al chat</a></p>');
 });
@@ -40,53 +42,59 @@ app.use('/', express.static(__dirname +'/'));
 //Este directorio publico contiene ficheros estáticos
 app.use(express.static('public'));
 
+function comandos_especiales(msg){
+    let data;
+    if(msg == '/help'){
+      console.log('Lista de comandos válidos');
+      data = commands;
+    }else if(msg == '/list'){
+      console.log('Devuelve el número de usuarios conectados');
+      data = "Hay " + user_count + " usuarios conectados en el chat";
+    }else if(msg == '/hello'){
+      console.log('El servidor devuelve el saludo');
+      data = hello_message;
+    }else if(msg == '/date'){
+      console.log('Devuelve la fecha');
+      data = "La fecha actual es: " + date.toLocaleDateString("es-ES", options);
+    }else{
+      console.log('Comando no valido');
+      data = ("Comando no válido. Introduce /help para mostrar los comandos permitidos en el chat");
+    };
+    return(data);
+  };
+ 
 //GESTION SOCKETS IO
 //Evento: Nueva conexion recibida
 io.on('connect', (socket) => {
 
   console.log('** NUEVA CONEXIÓN **'.yellow);
   user_count = user_count + 1;
-  //Enviamos mensaje bienvenida
+
+  //Mensaje bienvenida
   socket.send(welcome_message);
 
-  //Enviamos mensaje al resto de usuarios de que hay un nuevo usuario en el chat
+  //Mensaje al resto de usuarios de que hay un nuevo usuario en el chat
   io.send(new_user);
 
   //Evento desconexión
   socket.on('disconnect', function(){
     console.log('** CONEXIÓN TERMINADA **'.yellow);
     user_count -=1;
+
+    io.send(desconected);
  });  
 
   //Mensaje recibido: Hacer eco
   socket.on("message", (msg)=> {
     console.log("Mensaje Recibido!: " + msg.blue);
-    if (msg.startsWith('/')){
-        if (msg == '/help'){
-            console.log("Muestra una lista con todos los comandos soportados");
-            msg = commands;
-            socket.send(msg);
-        }else if (msg == '/list'){
-            console.log("Devuelve el número de usuarios conectados");
-            msg = ("El número de usuarios conectados es: " + user_count);
-            socket.send(msg);
-        }else if (msg == '/hello'){
-            console.log("El servidor devuelve el saludo");
-            msg = hello_message;
-            socket.send(msg);
-        } else if(msg == '/date'){
-            console.log("Devuelve la fecha");
-            msg = ("La fecha actual es: " + date);
-            socket.send(msg);
-        }else{
-            console.log("Comando no reconocido");
-            msg = ("Comando no reconocido");
-            socket.send(msg);
-        }
-
+    msg_text = msg.split(' ')[1];
+    if(msg_text.startsWith('/')){
+        console.log("Recurso recibido!: " + msg_text.red);
+        data = comandos_especiales(msg_text);
+        socket.send(data);
     }else{
-        io.send(msg);
-    }
+        io.send(msg); 
+    } 
   });
 });
 
--- P3/public/chat-client.js
+++ P3/public/chat-client.js
@@ -1,34 +1,42 @@
 //-- Elementos del interfaz
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
-const msg_nick = document.getElementById("nick");
+const msg_ = document.getElementById("msg_user");
 
-//Variable nombre usuario
-let nickname = 'Anonimo';
+let user = 'Desconocido';
+
+let write = false;
 
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
-
 
 socket.on("message", (msg)=>{
   display.innerHTML += '<p>' + msg + '</p>';
 });
 
-//-- Al apretar el botón se envía un mensaje al servidor
+//-- Se envia un mensaje al servidor al pulsar el botón
 msg_entry.onchange = () => {
   if (msg_entry.value)
-  socket.send(nickname + ': ' + msg_entry.value);
+  socket.send(user + ">> " + msg_entry.value);
+  write = false;
 
   //-- Borrar el mensaje actual
   msg_entry.value = "";
 }
 
-console.log(nickname)
+//Si se escribe, se mandará un mensaje a los demás usuarios
+msg_entry.oninput = () => {
+    if(!write){
+        write = true;
+        socket.send(user + ' está escribiendo');
+    };
+};
 
-//-- Al introducir el botón del nick se le asigna
-msg_nick.onchange = () => {
-    if(msg_nick.value){
-        nickname = msg_nick.value;
+console.log(user)
+
+//Cuando se introduce nombre usuario
+msg_user.onchange = () => {
+    if(msg_user.value){
+      user = msg_user.value;
     }
-    console.log(nickname);
-} 
+};
